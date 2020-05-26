4e56a2a7c4f2d2503c15e98bb7686a90d8232882
Huy Xu <nhathuyhedspi@gmail.com>
2020-05-26 11:27:10 +0700

    init project

---
+++ src/view/Main.java
@@ -0,0 +1,23 @@
+package view;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+  @Override
+  public void start(Stage primaryStage) throws Exception{
+    Group root = new Group();
+
+    primaryStage.setTitle("Visualize Tree Algorithms");
+    primaryStage.setScene(new Scene(root, 300, 275));
+    primaryStage.show();
+  }
+
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+}
